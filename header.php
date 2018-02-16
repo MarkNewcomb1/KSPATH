2c1a6a9101069881d117c1b4e81149ff0b1c329e
MarkNewcomb1 <wlmmn@hotmail.com>
2018-02-15 22:11:22 -0700

    purged old files for fresh start, now running sass

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <title>KS PATH</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Kansas Pathology">
+  <meta name="keywords" content="Kansas Pathology">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,300,600' rel='stylesheet' type='text/css'>
+
+  
+  <!-- Stylesheets -->
+  <link href="dist/css/style.min.css" rel="stylesheet">
+
+  
+  <!-- 2018 -->
+  <!-- HTML5 Support for IE -->
+  <!--[if lt IE 9]>
+  <script src="js/html5shim.js"></script>
+  <![endif]-->
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="img/favicon/favicon.png">
+</head>
