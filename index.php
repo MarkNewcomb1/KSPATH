8d12abdca407b3f4ad34c81458fef991e2deeacb
MarkNewcomb1 <wlmmn@hotmail.com>
2018-02-19 15:51:52 -0700

    gitignore ignored too much

---
+++ index.php
@@ -0,0 +1,180 @@
+<?php
+include 'header.php';	
+?>
+
+
+<!-- Slider starts -->
+    <div class="full-slider">
+        <!-- Slider (Flex Slider) -->
+          
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="flexslider">
+                  <ul class="slides">
+
+                    <li>
+                      
+                      <!-- Slider content -->
+                      <div class="flex-caption">
+                        <!-- Left column -->
+                        <div class="col-l">
+                          
+                            <h2>Quality Commitment</h2>
+
+                            <h6>We believe in the efficiency of quality. High quality results cost a little more to produce, but pay larger benefits in patient care. Our commitment to quality control and quality assurance has three components that overlap to assure accurate testing results.</h6>
+                     
+
+                        </div>
+                        <!-- Right column -->
+                        <div class="col-r">
+
+                          <!-- Use the class "flex-back" to add background inside flex slider -->
+                         
+                            <h5>Client Service</h5>
+                            <p>Our representatives will assure that your questions or concerns are promptly and courteously handled or channeled to the appropriate technical or professional personnel.</p>
+
+                        </div>
+                      </div>
+
+                    </li>
+
+                    <li>
+                      <div class="flex-caption flex-center">
+                        <h2>Testing Capabilities</h2>
+
+                        <p>Multi-speciality, Chemistry, Hematology, and Serology profiles, in addition to single tests, Endocrinology, and Therapeutic Drug Assay, services are available. </p>
+
+
+                      </div>
+                    </li>
+
+                    
+                    <li>
+                      
+                      <!-- Slider content -->
+                      <div class="flex-caption">
+                        <!-- Left column -->
+                        <div class="col-l">
+                          <h2>Quality Control</h2>
+                          <h6>This includes both external CAP proficiency surveys and also an internal quality control program. Coefficients of variation, standard deviations, standards and control monitors are analyzed to assure that the results are reliable.</h6>
+
+                        </div>
+                        <!-- Right column -->
+                        <div class="col-r">
+
+                          <div class="flex-back">
+                            <h5>We review statistical data</h5>
+                            <p>to verify that both our own internal and national external standards are met or exceeded.</p>
+                              <p>We voluntarily subscribe to the College of American Pathologists interlaboratory comparison and external proficiency program.</p>
+                              <p>All testing personnel in our laboratory participate in the proficiency program.</p>
+                          </div>
+
+                        </div>
+                      </div>
+
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+
+<!-- Slider Ends -->
+
+<div class="content">
+  <div class="container">
+
+
+    <!-- Hero Unit -->
+
+    <div class="row center">
+      <div class="col-md-12">
+
+        <div class="bor"></div>
+
+        <div class="bor"></div>
+        <hr />
+      </div>  
+    </div>
+
+    <!-- Hero Ends -->      
+
+
+    <div class="row">
+      <div class="col-md-12">
+
+        <div class="bor"></div>
+
+        <!-- Image blocks starts -->
+
+        <ul class="hover-block">
+
+          <li>
+
+            <a href="#">
+              <!-- Image -->
+              <img src="img/Microscope.gif" alt="" /> 
+              <!-- Content with background color Class -->
+              <div class="hover-content b-orange">
+                <h6>Stat Testing</h6>
+                Available on request for most tests. Please Client Service for detailed information on submission, delivery, and reporting. 
+              </div>
+            </a>
+
+          </li>
+
+
+          <li>
+
+            <a href="#">
+              <img src="img/Microscope.gif" alt="" /> 
+              <div class="hover-content b-red">
+                <h6>Call back Testing</h6>
+                Available on request. Specimens will be collected and processed as ROUTINE; however results will be called upon completion. 
+              </div>
+            </a>
+
+          </li>
+
+
+          <li>
+
+            <a href="#">
+              <img src="img/Microscope.gif" alt="" /> 
+              <div class="hover-content b-purple">
+                <h6>Routine Testing</h6>
+                will be collected and the test performed within 2 to 24 hours of receipt at KPS, unless otherwise at a predetermined time. 
+              </div>
+            </a>
+
+          </li>
+
+
+          <li>
+
+            <a href="#">
+              <img src="img/Microscope.gif" alt="" /> 
+              <div class="hover-content b-lblue">
+                <h6>Test Reporting</h6>
+                Written/electronic results are sent the quickest way possible, usually by Fax. On request, a report will be telephoned, then written.  
+              </div>
+            </a>
+
+          </li>            
+
+        </ul>
+
+        <div class="clearfix"></div>
+
+        <!-- Image blocks ends -->
+      </div>
+    </div>
+
+<div class="bor"></div>
+
+<hr />
+<?php
+include 'footer.php';
+?>
