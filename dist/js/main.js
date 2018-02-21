7d1cc203684e59742420986115bcefc4833588e1
MarkNewcomb1 <wlmmn@hotmail.com>
2018-02-20 20:39:31 -0700

    changed jquery nav selector to be more specific to the header nav

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	console.log("Hi there.");
-	$('nav').hide();
+	$('header nav').hide();
 	$('.mobile_menu_button').on('click', function(){
 		console.log("CLICK");
-		if ( $('nav').is(':hidden')) {
-			$('nav').slideDown(200);
+		if ( $('header nav').is(':hidden')) {
+			$('header nav').slideDown(200);
 		} else {
-			$('nav').slideUp(200);
+			$('header nav').slideUp(200);
 		}
 	}); //on click
 });
