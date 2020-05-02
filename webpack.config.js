60ffe8cd22a4b158ca24a99157f5bff17bb4e771
MarkNewcomb1 <wlmmn@hotmail.com>
2020-05-01 18:53:36 -0600

    it builds now - just into a big app.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
 const path = require('path');
 
 module.exports = {
-       mode: 'development',
-       entry: {
-         app: './index.js',
-           },
-       module: {
-		    rules: [
-		      {
-		        test: /\.s[ac]ss$/i,
-		        use: [
-		          // Creates `style` nodes from JS strings
-		          'style-loader',
-		          // Translates CSS into CommonJS
-		          'css-loader',
-		          // Compiles Sass to CSS
-		          'sass-loader',
-		        ],
-		      },
-		    ],
-       },
+	mode: 'development',
+	entry: {
+		app: './index.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-inline-loader'
+			}
+		],
+	},
 };
