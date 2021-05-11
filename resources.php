029eca4be9a4e83caea3a5ca40231d97b2f73482
MarkNewcomb1 <wlmmn@hotmail.com>
2021-05-11 10:31:16 -0600

    added link under resources

--- resources.php
+++ resources.php
@@ -23,6 +23,8 @@ include 'header.php';
 <h6>MICROBIOLOGY SPECIMEN COLLECTION AND TRANSPORT GUIDE:</h6>
 <a href="docs/microbiology-specimem-collection.docx" target="_blank">Click here</a>
 
+<h6>Billing and Coding: Lab: Controlled Substance Monitoring and Drugs of Abuse Testing</h6>
+<a href="https://na01.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.cms.gov%2Fmedicare-coverage-database%2Fdetails%2Farticle-details.aspx%3FarticleId%3D54799&data=04%7C01%7C%7Cc7fc4227ec8e401feb7808d913b9d489%7C84df9e7fe9f640afb435aaaaaaaaaaaa%7C1%7C0%7C637562511119133220%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=GX6IEdwWq0qgVPWEpep0sInLr7E8mS7a1I90xAUeuxM%3D&reserved=0" target="_blank">Click here</a>
 
     </div> <!-- col --> 
 </div> <!-- container -->
